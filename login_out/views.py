356f5a46e1007a6eea822f331eb69be76f8ea01e
Jim Munro <jimfmunro@gmail.com>
2012-02-07 06:35:34 -0700

    updated

--- login_out/views.py
+++ login_out/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth import login,logout
 from django.http import HttpResponse
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 
 def home(request):
-    vars = { 'user', request.user }
-    return render_to_response('main_page.html', vars)
+    return render_to_response('main_page.html', RequestContext(request))
 
 @login_required
 def login_req(request):
-    return render_to_response('login_required.html', {})
+    vars = RequestContext(request, { 'user', request.user } )
+    return render_to_response('login_required.html', vars)
