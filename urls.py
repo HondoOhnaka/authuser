f7921af171945f4e1e5a7d9e51c69c0b33bfa9b6
Jim Munro <jimfmunro@gmail.com>
2012-02-08 06:18:31 -0700

    authentication skeleton

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from authuser.login_out import views
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login, logout, logout_then_login
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
     
     (r'^accounts/login/$', login),
     (r'^login/$', login),
-    (r'^logout/$', logout),
+    (r'^accounts/login/$', login),
+    (r'^logout/$', logout_then_login),
     (r'^login-required/$', views.login_req),
 )
